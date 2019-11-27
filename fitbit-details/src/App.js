065f48dd1b354fd65c7e5346e7d5121421ce3b49
aakash sangwan <aakashsangwan024@gmail.com>
2019-11-27 19:08:54 +0530

    Product Page

--- fitbit-details/src/App.js
+++ fitbit-details/src/App.js
@@ -30,7 +30,7 @@ function App() {
          
          
          <div>
-           <img className={[classes.ProductImage,classes.SelectedProductImage].join( ' ')} src="https://imgur.com/iOeUBV7.png"  alt="Black colored Watch"></img>
+           <img className={[classes.ProductImage,classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png"  alt="Black colored Watch"></img>
            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red colored Watch"></img>
            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue colored Watch"></img>
            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple colored Watch"></img>
@@ -41,9 +41,11 @@ function App() {
            <h3 className={classes.SectionHeading}>Features</h3>
 
            <div>
-             <button className={classes.FeatureTime}>Time</button>
+             <button className={[classes.FeatureTime,classes.SelectedFeatureItem].join(' ')}>Time</button>
              <button className={classes.FeatureTime}>Heart Rate</button>
            </div> 
+
+           <button className={classes.PrimaryButton}>Buy Now</button>
 
        </div>
       </div> 
