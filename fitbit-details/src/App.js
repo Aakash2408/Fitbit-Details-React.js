283830941c38e44dfd9c0e0b6d3d20d2d80e7f04
aakash sangwan <aakashsangwan024@gmail.com>
2019-11-27 16:58:53 +0530

    Product Page

--- fitbit-details/src/App.js
+++ fitbit-details/src/App.js
@@ -21,8 +21,32 @@ function App() {
          <h1 className={classes.ProductTitle}>
            {ProductData.title}
          </h1>
+         <p className={classes.ProductDecription}>
+           {ProductData.description}
+         </p>
+
+
+         <h3 className={classes.SectionHeading}>Select Color</h3>
+         
+         
+         <div>
+           <img className={[classes.ProductImage,classes.SelectedProductImage].join( ' ')} src="https://imgur.com/iOeUBV7.png"  alt="Black colored Watch"></img>
+           <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red colored Watch"></img>
+           <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue colored Watch"></img>
+           <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple colored Watch"></img>
+           
+           </div>
+              
+
+           <h3 className={classes.SectionHeading}>Features</h3>
+
+           <div>
+             <button className={classes.FeatureTime}>Time</button>
+             <button className={classes.FeatureTime}>Heart Rate</button>
+           </div> 
+
        </div>
-      </div>
+      </div> 
     </div>
   );
 }
